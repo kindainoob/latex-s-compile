8ea5b7ad2315703315b1b338d2657b0c1c3882c8
kindainoob <1810370015r@kindai.ac.jp>
2018-08-18 10:57:19 +0900

    引数なし -> ファイル削除 から 引数あり -> コンパイル -> ファイル削除 引数なし -> -> ファイル削除に変更

--- compile.py
+++ compile.py
@@ -3,25 +3,21 @@ import os
 
 args = sys.argv
 
-# 引数が1つ(指定なし)ならログファイルの削除のみ
-if len(args) == 1:
-    os.system('rm ../*.log ../*.aux../*.dvi')
-    os.system('rm *.log *.aux *.dvi')
-    # 終了
-    sys.exit()
-# 引数の数を得る
-length = len(args)-1
-# 引数の数だけループ
-for num in range(length):
-    # 引数の1番目から
-    num += 1
-    filename = args[num]
+# 引数が11つでないならコンパイル
+if len(args) != 1:
+    # 引数の数を得る
+    length = len(args)-1
+    # 引数の数だけループ
+    for num in range(length):
+        # 引数の1番目から
+        num += 1
+        filename = args[num]
 
-    os.system('platex ../' + filename + '.tex')
-    os.system('platex ../' + filename + '.tex')
-    os.system('dvipdfmx ' + filename + '.dvi')
+        os.system('platex ../' + filename + '.tex')
+        os.system('platex ../' + filename + '.tex')
+        os.system('dvipdfmx ' + filename + '.dvi')
 
-    os.system('mv ' + filename + '.pdf ../')
+        os.system('mv ' + filename + '.pdf ../')
 
 os.system('rm ../*.log ../*.aux../*.dvi')
 os.system('rm *.log *.aux *.dvi')
